7fc2bff94f7e50af7d176bd610b22a76022b1c92
Frederik Nieß <1968126+zeroline@users.noreply.github.com>
2024-01-22 10:53:08 +0100

    Added TempFileHelper class

---
+++ src/Helper/TempFileHelper.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @author Frederik Nieß <miniloom@zeroline.me>
+ * @license MIT
+ * @package MiniLoom
+ * @subpackage Helper
+ *
+ * The TempFileHelper class provides some useful functions to create temporary files.
+ */
+
+namespace zeroline\MiniLoom\Helper;
+
+
+final class TempFileHelper
+{
+    /**
+     * Writes a string to a temporary file.
+     * @param string $data
+     * @param string $prefix
+     * @return null|string
+     */
+    public static function write(string $data, string $prefix = '') : ?string
+    {
+        $temp_file = tempnam(sys_get_temp_dir(), $prefix);
+        if (file_put_contents($temp_file, $data)) {
+            return $temp_file;
+        }
+        return null;
+    }
+
+    /**
+     * Reads a temporary file.
+     * @param string $filename
+     * @return string|bool
+     */
+    public static function read(string $filename) : string|bool
+    {
+        return file_get_contents($filename);
+    }
+}
