24844e1022b9a2b22a309b8f51ae3f2453eb516d
Frederik Nieß <1968126+zeroline@users.noreply.github.com>
2024-01-22 14:48:50 +0100

    Added test cases for DataContainer

---
+++ tests/Data/DataContainerTest.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @author Frederik Nieß <miniloom@zeroline.me>
+ * @package MiniLoom
+ * @subpackage Tests
+ * @license MIT
+ *
+ * Test case for DataContainer class
+ */
+
+namespace zeroline\MiniLoom\Tests\Data;
+
+use PHPUnit\Framework\TestCase;
+use zeroline\MiniLoom\Data\DataContainer as DataContainer;
+
+class DataContainerTest extends TestCase
+{
+    public function testContainerConstructor()
+    {
+        $data = new DataContainer(['test' => 'test']);
+        $this->assertEquals('test', $data->get('test'));
+        $this->assertEquals('test', $data->test);
+        $this->assertTrue(isset($data->test));
+
+        $this->assertFalse(isset($data->test2));
+    }
+
+    public function testContainerDynamicData()
+    {
+        $data = new DataContainer();
+        $data->test = 'test';
+        $this->assertEquals('test', $data->get('test'));
+        $this->assertEquals('test', $data->test);
+        $this->assertTrue(isset($data->test));
+
+        $this->assertFalse(isset($data->test2));
+    }
+
+    public function testReturnCompleteData() {
+        $data = new DataContainer(['test' => 'test']);
+        $this->assertEquals(['test' => 'test'], $data->getData());
+    }
+
+    public function testJsonSerializeImplementation() {
+        $data = new DataContainer(['test' => 'test']);
+        $this->assertEquals('{"test":"test"}', json_encode($data));
+    }
+}
