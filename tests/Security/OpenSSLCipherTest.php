5308dc1d2f17a0fdf3b5665892ffe02409ac73b4
Frederik Nieß <1968126+zeroline@users.noreply.github.com>
2024-01-22 14:28:31 +0100

    Added test cases for OpenSSL encryption and decryption

---
+++ tests/Security/OpenSSLCipherTest.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * @author Frederik Nieß <miniloom@zeroline.me>
+ * @package MiniLoom
+ * @subpackage Tests
+ * @license MIT
+ *
+ * Test case for HMAC
+ */
+
+namespace zeroline\MiniLoom\Tests\Security;
+
+use PHPUnit\Framework\TestCase;
+use zeroline\MiniLoom\Security\Encryption\OpenSSL\Crypter as Crypter;
+
+class OpenSSLCipherTest extends TestCase
+{
+    private const MESSAGE = 'MiniLoom says: Hello World! Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+    private const KEY = '1234567890';
+    private const INVALID_KEY = '0123456789';
+
+    public function testEncryptingAndDecrypting()
+    {
+        $encryptedMessage = Crypter::encrypt(self::MESSAGE, self::KEY);
+        $this->assertNotEmpty($encryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $encryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt($encryptedMessage, self::KEY);
+        $this->assertNotEmpty($decryptedMessage);
+        $this->assertEquals(self::MESSAGE, $decryptedMessage);
+    }
+
+    public function testEncryptingAndDecryptingWithInvalidKey()
+    {
+        $encryptedMessage = Crypter::encrypt(self::MESSAGE, self::KEY);
+        $this->assertNotEmpty($encryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $encryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt($encryptedMessage, self::INVALID_KEY);
+        $this->assertEmpty($decryptedMessage);
+        $this->assertFalse($decryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $decryptedMessage);
+    }
+
+    public function testEncryptingAndDecryptingWithInvalidTag()
+    {
+        $encryptedMessage = Crypter::encrypt(self::MESSAGE, self::KEY);
+        $this->assertNotEmpty($encryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $encryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt($encryptedMessage, self::KEY);
+        $this->assertNotEmpty($decryptedMessage);
+        $this->assertEquals(self::MESSAGE, $decryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt($encryptedMessage . 'invalid', self::KEY);
+        $this->assertEmpty($decryptedMessage);
+        $this->assertFalse($decryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $decryptedMessage);
+    }
+
+    public function testEncryptingAndDecryptingWithInvalidMessage()
+    {
+        $encryptedMessage = Crypter::encrypt(self::MESSAGE, self::KEY);
+        $this->assertNotEmpty($encryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $encryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt($encryptedMessage, self::KEY);
+        $this->assertNotEmpty($decryptedMessage);
+        $this->assertEquals(self::MESSAGE, $decryptedMessage);
+
+        $decryptedMessage = Crypter::decrypt('invalid'.$encryptedMessage, self::KEY);
+        $this->assertEmpty($decryptedMessage);
+        $this->assertFalse($decryptedMessage);
+        $this->assertNotEquals(self::MESSAGE, $decryptedMessage);
+    }
+    
+}
