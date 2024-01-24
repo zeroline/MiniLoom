11dbbe23dabf56e7c2db214b3406101584066b04
Frederik Nieß <1968126+zeroline@users.noreply.github.com>
2024-01-24 17:36:20 +0100

    SQLFileExecuter now returns the boolean result of the execution

--- src/Data/Database/SQL/SQLFileExecuter.php
+++ src/Data/Database/SQL/SQLFileExecuter.php
@@ -17,16 +17,18 @@ final class SQLFileExecuter
     /**
      * Read and execute an sql file. All found queries are executed
      * within a transaction environment. If one query fails no query will be
-     * commited
+     * commited.
+     * Transactions may not be suitable for all queries. Altering tables may not
+     * benefit from transactions.
      *
      * @param string $sqlFilename
      * @param string $connection
      *
      * @throws \Exception on script exec error
      *
-     * @return void
+     * @return bool
      */
-    public static function loadAndExecute(string $sqlFilename, bool $useTransaction = false, ?string $connection = null) : void
+    public static function loadAndExecute(string $sqlFilename, bool $useTransaction = false, ?string $connection = null) : bool
     {
         if (!is_readable($sqlFilename)) {
             throw new \Exception('SQL file "' . $sqlFilename . '" is not readable.');
@@ -73,7 +75,6 @@ final class SQLFileExecuter
             }
         }
 
-        // TODO: Table creation does not benefit from transactions
-        // MySQL does not rollback or commit that kind of executions
+        return $result;
     }
 }
