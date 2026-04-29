af5e410dc70ef800ffff33d423cd4409b81460f3
zeroline <zeroline@users.noreply.github.com>
2026-04-29 20:58:10 +0200

    Updated PredefinedContentTypeHeaders.php to ensure better maintenance

--- src/Controlling/HTTP/PredefinedContentTypeHeaders.php
+++ src/Controlling/HTTP/PredefinedContentTypeHeaders.php
@@ -17,29 +17,81 @@ class PredefinedContentTypeHeaders
     public const TEXT_PLAIN = 'text/plain';
     public const XML = 'application/xml';
 
-    public static function setHTMLHeader() : void
+    public const TRANSFER_BASE64 = 'base64';
+
+    private const HEADER_KEY_CONTENT_TYPE = 'Content-Type';
+    private const HEADER_KEY_CONTENT_TRANSFER = 'Content-Transfer-Encoding';
+    private const HEADER_DECLARATOR = ': ';
+
+    /**
+     * Internal helper function to set a header
+     * 
+     * @param string $header 
+     * @param string $value 
+     * @param bool $replace 
+     * @return void 
+     */
+    protected static function setCustomHeader(string $header, string $value, bool $replace = true): void
     {
-        header('Content-Type: ' . self::HTML, true);
+        header($header . self::HEADER_DECLARATOR . $value, $replace);
     }
 
-    public static function setJSONHeader() : void
+    /**
+     * Helper function to set the content header
+     * 
+     * @param string $contentType 
+     * @param bool $replace 
+     * @return void 
+     */
+    public static function setContentHeader(string $contentType, bool $replace = true): void
     {
-        header('Content-Type: ' . self::JSON, true);
+        self::setCustomHeader(self::HEADER_KEY_CONTENT_TYPE, $contentType, $replace);
     }
 
-    public static function setPlainTextHeader() : void
+    /**
+     * Simply set the header content type to HTML
+     * @return void 
+     */
+    public static function setHTMLHeader(): void
     {
-        header('Content-Type: ' . self::TEXT_PLAIN, true);
+        self::setContentHeader(self::HTML);
     }
 
-    public static function setXMLHeader() : void
+    /**
+     * Simply set the header content type to JSON
+     * @return void 
+     */
+    public static function setJSONHeader(): void
     {
-        header('Content-Type: ' . self::XML, true);
+        self::setContentHeader(self::JSON);
     }
 
-    public static function setJWTPlainHeader() : void
+    /**
+     * Simply set the header content type to plain TEXT
+     * @return void 
+     */
+    public static function setPlainTextHeader(): void
     {
-        header('Content-Type: ' . self::TEXT_PLAIN, true);
-        header("Content-Transfer-Encoding: base64", true);
+        self::setContentHeader(self::TEXT_PLAIN);
+    }
+
+    /**
+     * Simply set the header content type to XML
+     * @return void 
+     */
+    public static function setXMLHeader(): void
+    {
+        self::setContentHeader(self::XML);
+    }
+
+    /**
+     * Simply set the header content type to plain TEXT and
+     * additionaly set the content transfer to base64
+     * @return void 
+     */
+    public static function setJWTPlainHeader(): void
+    {
+        self::setPlainTextHeader();
+        self::setCustomHeader(self::HEADER_KEY_CONTENT_TRANSFER, self::TRANSFER_BASE64, true);
     }
 }
